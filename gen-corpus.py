884b28048ea217fbccf2eafba2b57fc41035c196
钟威 <zhongwei@dm-ai.cn>
2020-07-22 09:51:58 +0800

    gen-corpus.py: start generating training data using random expression.

--- gen-corpus.py
+++ gen-corpus.py
@@ -8,7 +8,7 @@ from common_axioms import common_axioms
 
 from mcts import mcts
 from dfs import dfs
-from test_cases import test_cases_x3_rational, test_cases_wiki131278697
+from test_cases import test_cases_x3_rational, test_cases_wiki131278697, test_case_from_log
 
 
 def generate_corpus(test_case_id, data, steps, DIV=20):
@@ -33,58 +33,69 @@ if __name__ == '__main__':
     # test cases to generate steps
     testcases = []
 
-    testcases += [
-        '\\frac{12a}{3a + a + 20a} - \\frac{1}{4}'
-    ]
+    #testcases += [
+    #    '\\frac{12a}{3a + a + 20a} - \\frac{1}{4}'
+    #]
 
-    tmp, _ = test_cases_x3_rational()
-    testcases += tmp
+    #tmp, _ = test_cases_x3_rational()
+    #testcases += tmp
 
-    tmp, _ = test_cases_wiki131278697()
-    testcases += tmp
+    #tmp, _ = test_cases_wiki131278697()
+    #testcases += tmp
+
+    testcases += test_case_from_log('./rational_8000.txt')
+    testcases += test_case_from_log('./full_random_800.txt')
 
     #n_sample_times = 220
     n_sample_times = 440
-    start = 72
-    always_use_MCTS = True
+    start = 0
+    always_use_MCTS = False
 
     open('fallback.log', 'w')
 
     for i, expr in enumerate(testcases[:]):
-        if i < 72: continue
+        if i < start: continue
 
-        narr = expression.tex2narr(expr)
+        try:
+            narr = expression.tex2narr(expr)
 
-        err = None
-        if not always_use_MCTS:
-            # use DFS or mcts (as fallback) to generate steps
-            steps, err = dfs(narr, basic_axioms, debug=True, maxsteps=150)
-            steps = [(n, a, ai) for n, an, a, ai in steps]
+            err = None
+            if not always_use_MCTS:
+                # use DFS or mcts (as fallback) to generate steps
+                steps, err = dfs(narr, basic_axioms, debug=True, maxsteps=150)
+                steps = [(n, a, ai) for n, an, a, ai in steps]
 
-        if err or always_use_MCTS:
-            with open('fallback.log', 'a') as fh:
-                fh.write(f'#{i}: ' + expr + '\n')
+            if err or always_use_MCTS:
+                with open('fallback.log', 'a') as fh:
+                    fh.write(f'#{i}: ' + expr + '\n')
 
-            steps = mcts(narr, all_axioms, debug=False, n_sample_times=n_sample_times,
-                nn_models=None, force_single_thread=False)
+                steps = mcts(narr, all_axioms, debug=False, n_sample_times=n_sample_times,
+                    nn_models=None, force_single_thread=False)
 
-        # make data pair
-        data = []
-        for j in range(1, len(steps)):
-            last_narr, _, _ = steps[j - 1]
-            narr, axiom, axiom_idx = steps[j]
+            # make data pair
+            data = []
+            for j in range(1, len(steps)):
+                last_narr, _, _ = steps[j - 1]
+                narr, axiom, axiom_idx = steps[j]
 
-            last_expr = expression.narr2tex(last_narr)
-            expr = expression.narr2tex(narr)
+                last_expr = expression.narr2tex(last_narr)
+                expr = expression.narr2tex(narr)
 
-            axiom_name = axiom.name()
-            rich.print(f'step{j} axiom#{axiom_idx} {axiom_name}', expr)
+                axiom_name = axiom.name()
+                rich.print(f'step{j} axiom#{axiom_idx} {axiom_name}', expr)
 
-            data.append((last_expr, axiom_idx + 1, expr))
-        data.append((expr, 0, expr))
+                data.append((last_expr, axiom_idx + 1, expr))
+            data.append((expr, 0, expr))
 
-        # write data pair
-        print(f'Test case: {i} / {len(testcases) - 1}')
-        generate_corpus(i, data, steps, DIV=20)
+            # write data pair
+            print(f'Test case: {i} / {len(testcases) - 1}')
+            generate_corpus(i, data, steps, DIV=20)
 
-        #input('pause')
+            #input('pause')
+
+        except KeyboardInterrupt:
+            print('Abort')
+            quit()
+        except Exception as err:
+            print(err)
+            continue
