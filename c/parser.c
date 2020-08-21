8b847b7b518c3bd7b292caf51cd0462789643185
钟威 <zhongwei@dm-ai.cn>
2020-08-21 19:02:28 +0800

    apply parsing...

--- c/parser.c
+++ c/parser.c
@@ -2,9 +2,22 @@
 #include "lex.yy.h"
 
 #include "optr.h"
+#include "mhook.h"
 
 int main()
 {
 	yyscan_t scanner;
-	yylex_init(&scanner);
+	if (yylex_init(&scanner))
+		return 1;
+	
+	YY_BUFFER_STATE buf = NULL;
+
+	buf = yy_scan_string("1 + 2", scanner);
+	yyparse(scanner);
+
+	yy_delete_buffer(buf, scanner);
+	yylex_destroy(scanner);
+
+	mhook_print_unfree();
+	return 0;
 }
